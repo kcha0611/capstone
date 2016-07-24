d2654c60da36550e1bb973015f0dc90b643fc495
Ken Cha <kencha0611@gmail.com>
2016-07-24 12:29:45 -0700

    fixed https

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -57955,7 +57955,7 @@
 	            )
 	          )
 	        ),
-	        React.createElement('img', { src: 'http://res.cloudinary.com/dllnnnotc/image/upload/c_fit,e_sharpen,h_800,q_100,w_1229/v1469382975/lakes-hd-wallpapers-awesome-beautiful-hd-high-quality-nature-desktop-widescreen-backgrounds_ogya4v.jpg', id: 'carousel-img1' })
+	        React.createElement('img', { src: 'https://res.cloudinary.com/dllnnnotc/image/upload/c_fit,e_sharpen,h_800,q_100,w_1229/v1469382975/lakes-hd-wallpapers-awesome-beautiful-hd-high-quality-nature-desktop-widescreen-backgrounds_ogya4v.jpg', id: 'carousel-img1' })
 	      ),
 	      React.createElement(
 	        Carousel.Item,
@@ -57974,7 +57974,7 @@
 	            )
 	          )
 	        ),
-	        React.createElement('img', { src: 'http://res.cloudinary.com/dllnnnotc/image/upload/c_scale,h_800,w_1229/v1469382309/free-food-wallpaper-hd-4_jjbzmd.jpg', id: 'carousel-img2' })
+	        React.createElement('img', { src: 'https://res.cloudinary.com/dllnnnotc/image/upload/c_scale,h_800,w_1229/v1469382309/free-food-wallpaper-hd-4_jjbzmd.jpg', id: 'carousel-img2' })
 	      ),
 	      React.createElement(
 	        Carousel.Item,
@@ -57993,7 +57993,7 @@
 	            )
 	          )
 	        ),
-	        React.createElement('img', { src: 'http://res.cloudinary.com/dllnnnotc/image/upload/c_scale,h_800,w_1229/v1469382328/photo-1446776811953-b23d57bd21aa_abkuii.jpg', id: 'carousel-img3' })
+	        React.createElement('img', { src: 'https://res.cloudinary.com/dllnnnotc/image/upload/c_scale,h_800,w_1229/v1469382328/photo-1446776811953-b23d57bd21aa_abkuii.jpg', id: 'carousel-img3' })
 	      )
 	    );
 	  }
--- frontend/components/carousel.jsx
+++ frontend/components/carousel.jsx
@@ -14,19 +14,19 @@ const SlideShow = React.createClass({
           <Carousel.Caption>
             <h3 id="carousel-renewable">Let's Make<p id="carousel-text1">Renewables</p></h3>
           </Carousel.Caption>
-            <img src="http://res.cloudinary.com/dllnnnotc/image/upload/c_fit,e_sharpen,h_800,q_100,w_1229/v1469382975/lakes-hd-wallpapers-awesome-beautiful-hd-high-quality-nature-desktop-widescreen-backgrounds_ogya4v.jpg" id="carousel-img1"></img>
+            <img src="https://res.cloudinary.com/dllnnnotc/image/upload/c_fit,e_sharpen,h_800,q_100,w_1229/v1469382975/lakes-hd-wallpapers-awesome-beautiful-hd-high-quality-nature-desktop-widescreen-backgrounds_ogya4v.jpg" id="carousel-img1"></img>
         </Carousel.Item>
         <Carousel.Item>
           <Carousel.Caption>
             <h3 id="carousel-wearable">Let's Make <p id="carousel-text2">Food</p></h3>
           </Carousel.Caption>
-          <img src="http://res.cloudinary.com/dllnnnotc/image/upload/c_scale,h_800,w_1229/v1469382309/free-food-wallpaper-hd-4_jjbzmd.jpg" id="carousel-img2"></img>
+          <img src="https://res.cloudinary.com/dllnnnotc/image/upload/c_scale,h_800,w_1229/v1469382309/free-food-wallpaper-hd-4_jjbzmd.jpg" id="carousel-img2"></img>
         </Carousel.Item>
         <Carousel.Item>
           <Carousel.Caption>
             <h3 id="carousel-space">Let's Make <p id="carousel-text3">Space</p></h3>
           </Carousel.Caption>
-            <img src="http://res.cloudinary.com/dllnnnotc/image/upload/c_scale,h_800,w_1229/v1469382328/photo-1446776811953-b23d57bd21aa_abkuii.jpg" id="carousel-img3"></img>
+            <img src="https://res.cloudinary.com/dllnnnotc/image/upload/c_scale,h_800,w_1229/v1469382328/photo-1446776811953-b23d57bd21aa_abkuii.jpg" id="carousel-img3"></img>
         </Carousel.Item>
       </Carousel>
     )
